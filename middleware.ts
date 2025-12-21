38ba5a0e94e7107068c5ba84d864c19dd494b52d
Jin_Y <wlsdud6221@naver.com>
2025-12-22 01:27:04 +0900

    feat : 미들웨어 초안 작성 #1

---
+++ middleware.ts
@@ -0,0 +1,15 @@
+import { type NextRequest, NextResponse } from 'next/server'
+
+const matchersForAuthUser = ['/']
+const matchersForSignIn = ['/login/*', '/regist/*']
+
+function isMatch(pathname: string, urls: string[]) {
+  return urls.map(url => pathname.startsWith(url.replace('/*', '')))
+}
+
+export async function middleware(request: NextRequest) {
+  const { pathname } = request.nextUrl
+  console.log('체크')
+
+  return NextResponse.next()
+}
