1a15311aa743fd5ff3ab4213c43811d028235800
JinY <wlsdud6221@naver.com>
2026-01-02 02:57:47 +0900

    design : 연구실 참여 전 모달 디자인 #5

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -19,7 +19,9 @@ export default async function AuthLayout({
     return (
       <main className={'flex h-full w-full bg-white'}>
         <div className={'h-full flex-1 bg-[url("/홍보1.png")] bg-cover bg-center bg-no-repeat'} />
-        <div className={'flex h-full w-[730px] flex-col items-center justify-center gap-[30px] overscroll-auto'}>
+        <div
+          className={'relative flex h-full w-[730px] flex-col items-center justify-center gap-[30px] overscroll-auto'}
+        >
           <h1 className={'font-pretendard text-gray900 w-[400px] text-[48px] font-bold'}>MyLab</h1>
           {children}
         </div>
---
+++ src/widgets/modal/index.ts
@@ -0,0 +1,1 @@
+export { StuLabModal } from './ui/stu-lab-modal'
