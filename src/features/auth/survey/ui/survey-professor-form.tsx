18a31575ad026e96c8f07f5cbeebabffe0c36614
JinY <wlsdud6221@naver.com>
2026-01-02 02:56:41 +0900

    feat : 설문 기본 동작 구현 #5

--- src/features/auth/survey/ui/survey-professor-form.tsx
+++ src/features/auth/survey/ui/survey-professor-form.tsx
@@ -1,18 +1,43 @@
+'use client'
+
+import { useProfessor } from '@/features/auth/survey/model/useProfessor'
 import { Button, Input, P } from '@/shared/ui'
 
 export const ProfessorForm = () => {
+  const { data, isInput, handleChangeData } = useProfessor()
   return (
     <>
       <P className={'text-[32px] font-semibold whitespace-pre-wrap'}>{'김땡땡 님,\n연구실을 생성하세요'}</P>
       <form id={'create-lab-form'} action="" className={'flex w-[400px] flex-col gap-[20px]'}>
-        <Input type={'text'} className={'w-full'} placeholder={'학교 이름'} />
-        <Input type={'text'} className={'w-full'} placeholder={'학과 이름'} />
-        <Input type={'text'} className={'w-full'} placeholder={'연구실 이름'} />
+        <Input
+          type={'text'}
+          className={'w-full'}
+          placeholder={'학교 이름'}
+          name={'schoolName'}
+          value={data.schoolName}
+          onChange={handleChangeData}
+        />
+        <Input
+          type={'text'}
+          className={'w-full'}
+          placeholder={'학과 이름'}
+          name={'departmentName'}
+          value={data.departmentName}
+          onChange={handleChangeData}
+        />
+        <Input
+          type={'text'}
+          className={'w-full'}
+          placeholder={'연구실 이름'}
+          name={'labName'}
+          value={data.labName}
+          onChange={handleChangeData}
+        />
       </form>
-      <Button form={'create-lab-form'} variant={'noActive'}>
+      <Button form={'create-lab-form'} variant={isInput() ? 'primary' : 'noActive'}>
         연구실 생성하기
       </Button>
-      <P className={'text-[18px] font-medium'}>교수 인증은 평균 2일 이내에 처리됩니다.</P>
+      <P className={'text-[18px] font-medium'}>연구실 검토는 평균 2일 이내에 처리됩니다.</P>
     </>
   )
 }
--- src/features/auth/survey/ui/survey-student-form.tsx
+++ src/features/auth/survey/ui/survey-student-form.tsx
@@ -1,13 +1,24 @@
+'use client'
+
+import { useState } from 'react'
 import { Button, Input, P } from '@/shared/ui'
+import { StuLabModal } from '@/widgets/modal'
 
 export const StudentForm = () => {
+  const [hasCode, setHasCode] = useState<boolean>(false)
   return (
     <>
+      {/*<StuLabModal />*/}
       <P className={'text-[32px] font-semibold whitespace-pre-wrap'}>{'김땡땡 님,\n연구실에 참여하세요'}</P>
       <form id={'join-lab-form'} action="" className={'flex w-[400px] flex-col gap-[20px]'}>
-        <Input type={'text'} className={'w-full'} placeholder={'초대코드'} />
+        <Input
+          type={'text'}
+          className={'w-full'}
+          placeholder={'초대코드'}
+          onChange={e => setHasCode(e.target.value.trim().length > 0)}
+        />
       </form>
-      <Button form={'join-lab-form'} variant={'noActive'}>
+      <Button form={'join-lab-form'} variant={hasCode ? 'primary' : 'noActive'}>
         연구실 참여하기
       </Button>
     </>
