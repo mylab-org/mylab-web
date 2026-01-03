784a482d6ec2b8409eab3d81d45b2517857e2866
JinY <wlsdud6221@naver.com>
2026-01-04 02:55:38 +0900

    refactor : 회원 롤 선택 로직 추가 #4

--- src/features/auth/regist/ui/regist-form.tsx
+++ src/features/auth/regist/ui/regist-form.tsx
@@ -1,21 +1,57 @@
-import { Input, P } from '@/shared/ui'
+'use client'
+
+import { useRef } from 'react'
+import { FloatingLabel, Radio } from '@/shared/ui'
 
 export const RegistForm = () => {
+  const degreeRef = useRef(null)
+
+  const handleChangeRole = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (!degreeRef.current) return
+
+    degreeRef.current.style.display = e.target.value === 'student' ? 'flex' : 'none'
+  }
+
   return (
     <form id={'login-form'} action="" className={'flex w-[400px] flex-col gap-[20px]'}>
-      <Input type={'text'} placeholder={'이름'} />
-      <div>
-        <Input type={'text'} className={'w-full'} placeholder={'이메일'} />
-        <P className={'!text-error text-[12px]'}>이미 사용 중인 아이디입니다</P>
+      <div className={'flex gap-[20px]'}>
+        <Radio
+          id={'survey1'}
+          name={'role'}
+          value={'professor'}
+          title={'교수'}
+          defaultChecked
+          onChange={handleChangeRole}
+        />
+        <Radio id={'survey2'} name={'role'} value={'student'} title={'연구원'} onChange={handleChangeRole} />
       </div>
-      <div>
-        <Input type={'password'} className={'w-full'} placeholder={'비밀번호'} />
-        <P className={'!text-error text-[12px]'}>비밀번호는 8자 이상이어야 합니다</P>
+      <div ref={degreeRef} className={'hidden gap-[20px]'}>
+        <Radio id={'survey3'} name={'student'} title={'학부생'} defaultChecked />
+        <Radio id={'survey4'} name={'student'} title={'석사'} />
+        <Radio id={'survey5'} name={'student'} title={'박사'} />
       </div>
-      <div>
-        <Input type={'password'} className={'w-full'} placeholder={'비밀번호 확인'} />
-        <P className={'!text-error text-[12px]'}>비밀번호가 일치하지 않습니다</P>
-      </div>
+      <FloatingLabel type={'text'} labelName={'이름'} name={'name'} className={'w-full'} />
+      <FloatingLabel
+        type={'text'}
+        labelName={'이메일'}
+        name={'email'}
+        className={'w-full'}
+        errorMsg={'이미 사용 중인 아이디입니다'}
+      />
+      <FloatingLabel
+        type={'password'}
+        labelName={'비밀번호'}
+        name={'password'}
+        className={'w-full'}
+        errorMsg={'비밀번호는 8자 이상이어야 합니다'}
+      />
+      <FloatingLabel
+        type={'password'}
+        labelName={'비밀번호 확인'}
+        name={'passwordCheck'}
+        className={'w-full'}
+        errorMsg={'비밀번호가 일치하지 않습니다'}
+      />
     </form>
   )
 }
--- src/views/auth/RegistPage.tsx
+++ src/views/auth/RegistPage.tsx
@@ -5,15 +5,15 @@ import { AuthLinkItem } from '@/widgets/auth'
 export const RegistPage = () => {
   return (
     <div className={'flex flex-col gap-[20px]'}>
-      <div className={'flex gap-[20px]'}>
-        <Radio id={'survey1'} name={'survey'} title={'교수'} />
-        <Radio id={'survey2'} name={'survey'} title={'연구원'} />
-      </div>
-      <div className={'flex gap-[20px]'}>
-        <Radio id={'survey3'} name={'student'} title={'학부생'} />
-        <Radio id={'survey4'} name={'student'} title={'석사'} />
-        <Radio id={'survey5'} name={'student'} title={'박사'} />
-      </div>
+      {/*<div className={'flex gap-[20px]'}>*/}
+      {/*  <Radio id={'survey1'} name={'survey'} title={'교수'} />*/}
+      {/*  <Radio id={'survey2'} name={'survey'} title={'연구원'} />*/}
+      {/*</div>*/}
+      {/*<div className={'flex gap-[20px]'}>*/}
+      {/*  <Radio id={'survey3'} name={'student'} title={'학부생'} />*/}
+      {/*  <Radio id={'survey4'} name={'student'} title={'석사'} />*/}
+      {/*  <Radio id={'survey5'} name={'student'} title={'박사'} />*/}
+      {/*</div>*/}
       <RegistForm />
       <Button form={'login-form'} variant={'noActive'}>
         가입하기
