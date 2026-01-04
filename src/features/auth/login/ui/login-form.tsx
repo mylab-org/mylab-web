df362cc5cc143a33e6b578ee0c7a144afc96dc6c
JinY <wlsdud6221@naver.com>
2026-01-04 22:26:19 +0900

    refactor : 플로팅 라벨 적용 #4

--- src/features/auth/login/ui/login-form.tsx
+++ src/features/auth/login/ui/login-form.tsx
@@ -3,7 +3,7 @@
 import clsx from 'clsx'
 import { useActionState } from 'react'
 import { handleLoginAction } from '@/features/auth/login/model/login-action'
-import { Input, P } from '@/shared/ui'
+import { FloatingLabel, Input, P } from '@/shared/ui'
 
 export const LoginForm = () => {
   const initialState = { success: false, idMsg: '', pwMsg: '' }
@@ -16,17 +16,25 @@ export const LoginForm = () => {
         e.preventDefault()
         dispatch(new FormData(e.currentTarget))
       }}
-      className={'flex w-[400px] flex-col'}
+      className={'flex w-[400px] flex-col gap-[20px]'}
     >
-      <Input className={clsx(state.idMsg && '!border-b-error')} type={'text'} name={'email'} placeholder={'이메일'} />
-      <P className={'!text-error text-[12px]'}>{state.idMsg}</P>
-      <Input
-        className={clsx(state.pwMsg && '!border-b-error')}
-        type={'password'}
+      {/*<Input className={clsx(state.idMsg && '!border-b-error')} type={'text'} name={'email'} placeholder={'이메일'} />*/}
+      {/*<P className={'!text-error text-[12px]'}>{state.idMsg}</P>*/}
+      {/*<Input*/}
+      {/*  className={clsx(state.pwMsg && '!border-b-error')}*/}
+      {/*  type={'password'}*/}
+      {/*  name={'password'}*/}
+      {/*  placeholder={'비밀번호'}*/}
+      {/*/>*/}
+      {/*<P className={'!text-error text-[12px]'}>{state.pwMsg}</P>*/}
+      <FloatingLabel labelName={'이메일'} name={'email'} type={'text'} isError={!!state.idMsg} errorMsg={state.idMsg} />
+      <FloatingLabel
+        labelName={'비밀번호'}
         name={'password'}
-        placeholder={'비밀번호'}
+        type={'password'}
+        isError={!!state.pwMsg}
+        errorMsg={state.pwMsg}
       />
-      <P className={'!text-error text-[12px]'}>{state.pwMsg}</P>
     </form>
   )
 }
