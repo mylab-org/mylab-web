2ca2a5de2dd110208c0838bb23972867ffc331d3
JinY <wlsdud6221@naver.com>
2026-01-23 01:23:02 +0900

    design : 공통 태그 UI 수정 #9

--- src/shared/ui/tag.tsx
+++ src/shared/ui/tag.tsx
@@ -1,10 +1,24 @@
+import clsx from 'clsx'
 import { TAG_STYLE } from '@/shared/constant/TAG_STYLE'
 
-interface TagProps {
+interface RollTagProps {
   variant: keyof typeof TAG_STYLE
 }
 
-export const Tag = ({ variant }: TagProps) => {
+interface WorkTagProps {
+  children: React.ReactNode
+  isActive: boolean
+}
+
+const LabTag = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <span className={'font-pretendard bg-gray100 rounded-full px-[10px] py-[4px] text-[14px] font-medium'}>
+      {children}
+    </span>
+  )
+}
+
+const RollTag = ({ variant }: RollTagProps) => {
   const { bg, text, label } = TAG_STYLE[variant]
   return (
     <span className={`font-pretendard w-fit rounded-full ${bg} ${text} px-[10px] py-[3px] text-[14px] font-medium`}>
@@ -12,3 +26,22 @@ export const Tag = ({ variant }: TagProps) => {
     </span>
   )
 }
+
+const WorkTag = ({ isActive, children }: WorkTagProps) => {
+  return (
+    <span
+      className={clsx(
+        'font-pretendard rounded-[20px] px-[8px] py-[4px] text-[12px]',
+        isActive ? 'bg-blue-100 font-semibold text-blue-800' : 'bg-gray100 text-gray400',
+      )}
+    >
+      {children}
+    </span>
+  )
+}
+
+export const Tag = Object.assign(() => null, {
+  Roll: RollTag,
+  Lab: LabTag,
+  Work: WorkTag,
+})
