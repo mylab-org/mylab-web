29fd67c1b5265969be56a4d349dfecd2f0d47b23
JinY <wlsdud6221@naver.com>
2026-02-25 01:05:51 +0900

    feat : 탭 UI 정의 #22

---
+++ src/widgets/tab/ui/tab.tsx
@@ -0,0 +1,44 @@
+import clsx from 'clsx'
+import { useState } from 'react'
+
+interface TabItem {
+  name: string
+  content: React.ReactNode
+}
+
+interface TabsProps {
+  tabs: TabItem[]
+  leftItem?: React.ReactNode
+}
+
+export const Tab = ({ tabs, leftItem }: TabsProps) => {
+  const [currentTab, setCurrentTab] = useState<string>(tabs[0].name)
+
+  const activeTab = tabs.find(tab => tab.name === currentTab)
+
+  return (
+    <>
+      <div className={'flex items-center justify-between'}>
+        <div className={'flex'}>
+          {tabs.map(tab => {
+            const isActive = currentTab === tab.name
+
+            return (
+              <button
+                className={clsx(
+                  'font-pretendard cursor-pointer rounded-[10px] px-[15px] py-[10px] text-[18px] font-semibold',
+                  isActive ? 'bg-gray100' : 'text-gray400!',
+                )}
+                onClick={() => setCurrentTab(tab.name)}
+              >
+                {tab.name}
+              </button>
+            )
+          })}
+        </div>
+        {leftItem && leftItem}
+      </div>
+      {activeTab?.content}
+    </>
+  )
+}
