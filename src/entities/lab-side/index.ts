37da4103182d96eab94d7494502f3d0a4e70220e
JinY <wlsdud6221@naver.com>
2026-02-25 01:28:26 +0900

    design : 비품 신청 퍼블리싱 #22

--- src/entities/lab-side/index.ts
+++ src/entities/lab-side/index.ts
@@ -1,2 +1,3 @@
 export { SideManagenemtList } from './ui/side-managenemt-list'
 export { SidePostList } from './ui/side-post-list'
+export { SideStuPostList } from './ui/side-stu-post-list'
--- src/widgets/lab-side-content/ui/side-post.tsx
+++ src/widgets/lab-side-content/ui/side-post.tsx
@@ -1,10 +1,23 @@
-import { SearchInput } from '@/shared/ui'
+import { SideStuPostList } from '@/entities/lab-side'
+import { Button } from '@/shared/ui'
+import { Tab } from '@/widgets/tab'
 
 export const SidePost = () => {
+  const tabs = [
+    {
+      name: '비품 신청 내역',
+      content: <SideStuPostList />,
+    },
+    {
+      name: '비품 승인 내역',
+      content: <SideStuPostList />,
+    },
+  ]
+
   return (
-    <div className={'flex w-full flex-1 flex-col gap-[10px] py-[20px] lg:w-[700px]'}>
-      <SearchInput />
-      <div className={'flex-1'}>비품신청</div>
+    <div className={'flex w-full flex-1 flex-col gap-[10px] lg:w-[700px]'}>
+      <Tab tabs={tabs} />
+      <Button className={'mb-[20px]'}>비품 신청하기</Button>
     </div>
   )
 }
