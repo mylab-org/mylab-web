2569b9845a6b2d85793bdda063137ae195f1ccf6
JinY <wlsdud6221@naver.com>
2026-02-24 23:59:14 +0900

    feat : 오버레이 클릭 시 모달 닫힘 #22

--- src/widgets/modal/ui/side-modal-wrapper.tsx
+++ src/widgets/modal/ui/side-modal-wrapper.tsx
@@ -6,11 +6,16 @@ import { SideModalHeader } from '@/widgets/header'
 
 export const SideModalWrapper = () => {
   const isSideOpen = useSideModalStore(state => state.isSideOpen)
+  const closeSideModal = useSideModalStore(state => state.closeSideModal)
   const Content = useSideModalStore(state => state.selectedContent)
   useLockBodyScroll(isSideOpen)
 
+  const handleCloseSideModal = (e: React.MouseEvent<HTMLElement>) => {
+    if (e.target === e.currentTarget) closeSideModal()
+  }
+
   return (
-    <section className={'fixed inset-0 z-10 flex h-dvh w-full justify-end bg-black/10'}>
+    <section className={'fixed inset-0 z-10 flex h-dvh w-full justify-end bg-black/10'} onClick={handleCloseSideModal}>
       <div className={'flex w-full flex-col bg-white px-[30px] shadow-lg lg:w-fit lg:rounded-l-[20px]'}>
         <SideModalHeader />
         {Content && <Content />}
