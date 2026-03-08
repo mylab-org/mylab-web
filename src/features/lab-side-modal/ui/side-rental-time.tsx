dce5e2106f0d54fa011ecc607cdd81ff39b64242
JinY <wlsdud6221@naver.com>
2026-03-08 18:30:41 +0900

    feat : 타임피커 적용 #26 - 우선 기능은 제외하고 디자인만 구현

--- src/features/lab-side-modal/ui/side-rental-time.tsx
+++ src/features/lab-side-modal/ui/side-rental-time.tsx
@@ -1,4 +1,5 @@
 import { Button, Text } from '@/shared/ui'
+import TimePicker from '@/widgets/lab-side-content/ui/TimePicker'
 
 export const SideRentalTime = () => {
   return (
@@ -18,10 +19,11 @@ export const SideRentalTime = () => {
         </div>
       </div>
       <div className={'flex flex-1 items-center justify-center gap-[40px] p-[10px]'}>
-        <Text className={'text[20px] w-[120px] text-center font-medium lg:text-[30px]'}>오늘 (토)</Text>
-        <Text className={'text[22px] w-[60px] text-center font-medium lg:text-[34px]'}>02</Text>
-        <Text className={'text-[15px] font-medium lg:text-[20px]'}>:</Text>
-        <Text className={'text[22px] w-[60px] text-center font-medium lg:text-[34px]'}>40</Text>
+        {/*<Text className={'text[20px] w-[120px] text-center font-medium lg:text-[30px]'}>오늘 (토)</Text>*/}
+        {/*<Text className={'text[22px] w-[60px] text-center font-medium lg:text-[34px]'}>02</Text>*/}
+        {/*<Text className={'text-[15px] font-medium lg:text-[20px]'}>:</Text>*/}
+        {/*<Text className={'text[22px] w-[60px] text-center font-medium lg:text-[34px]'}>40</Text>*/}
+        <TimePicker />
       </div>
       <Button className={'bg-blue500! mt-[10px] mb-[20px]'}>총 2시간 대여</Button>
     </div>
