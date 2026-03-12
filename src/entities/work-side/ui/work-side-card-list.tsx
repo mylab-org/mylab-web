fb2bdcc956ff7eeec174306f2349ad59a34bcde2
JinY <wlsdud6221@naver.com>
2026-03-13 01:10:56 +0900

    refactor : 업무 상세 모바일 UI 수정 #15

---
+++ src/entities/work-side/ui/work-side-card-list.tsx
@@ -0,0 +1,20 @@
+import { WorkMyProgress } from '@/entities/works'
+import { Tag, Text } from '@/shared/ui'
+
+export const WorkSideCardList = () => {
+  return (
+    <div className={'flex min-h-0 flex-1 flex-col gap-[30px] overflow-y-auto border-t border-t-gray-300 py-[20px]'}>
+      <div className={'flex flex-col gap-[20px]'}>
+        <div className={'flex items-center gap-[10px]'}>
+          <div className={'h-[25px] w-[25px] rounded-full bg-gray-200'} />
+          <Text className={'text-[14px] font-bold md:text-[18px]'}>홍길동</Text>
+          <div className={'flex gap-[4px]'}>
+            <Tag.Roll variant={'leader'} />
+            <Tag.Roll variant={'doctoral'} />
+          </div>
+        </div>
+        <WorkMyProgress />
+      </div>
+    </div>
+  )
+}
