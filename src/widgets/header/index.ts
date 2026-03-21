1298139341d1b49a44b8324768a378dc7f07f573
JinY <wlsdud6221@naver.com>
2026-03-22 01:13:19 +0900

    remove : side header 통합 #29

--- src/widgets/header/index.ts
+++ src/widgets/header/index.ts
@@ -1,3 +1,2 @@
 export { PcHeader } from './ui/pc-header'
 export { MobileHeader } from './ui/mobile-header'
-export { SideModalHeader } from './ui/side-modal-header'
--- src/widgets/modal/ui/side-wrap.tsx
+++ src/widgets/modal/ui/side-wrap.tsx
@@ -1,23 +1,26 @@
 'use client'
 
 import { motion, AnimatePresence } from 'framer-motion'
-import { usePathname } from 'next/dist/client/components/navigation'
+import { Image } from 'next/dist/client/image-component'
+import { useEffect } from 'react'
 import { useLockBodyScroll } from '@/shared/model'
-import { useSideModalStore } from '@/shared/store/useSideModalStore'
-import { SideModalHeader } from '@/widgets/header'
+import { useSideModalStore } from '@/shared/store'
 
 export const SideWrap = () => {
   const isSideOpen = useSideModalStore(state => state.isSideOpen)
   const closeSideModal = useSideModalStore(state => state.closeSideModal)
   const Content = useSideModalStore(state => state.selectedContent)
+  const Title = useSideModalStore(state => state.sideTitle)
+
   useLockBodyScroll(isSideOpen)
+
+  useEffect(() => {
+    return () => closeSideModal()
+  }, [])
 
   const handleCloseSideModal = (e: React.MouseEvent<HTMLElement>) => {
     if (e.target === e.currentTarget) closeSideModal()
   }
-
-  const pathname = usePathname()
-  console.log('pathname', pathname)
 
   return (
     <AnimatePresence>
@@ -31,7 +34,7 @@ export const SideWrap = () => {
           transition={{ duration: 0.2 }}
         >
           <motion.div
-            className={`relative flex w-full flex-col bg-white shadow-lg lg:rounded-l-[20px] ${pathname === '/more' ? 'lg:w-[550px]' : 'lg:w-[750px]'}`}
+            className={`relative flex w-full flex-col bg-white shadow-lg lg:w-fit lg:rounded-l-[20px]`}
             initial={{ x: '100%' }}
             animate={{ x: 0 }}
             exit={{ x: '100%' }}
@@ -40,7 +43,17 @@ export const SideWrap = () => {
               ease: [0.22, 1, 0.36, 1], // iOS 느낌
             }}
           >
-            <SideModalHeader />
+            <div className={'flex items-center justify-between px-[20px] py-[20px] lg:px-[30px]'}>
+              <h3 className={'text-[18px] leading-[32px] font-bold lg:text-[24px]'}>{Title}</h3>
+              <Image
+                src={'icon/x.svg'}
+                alt={''}
+                width={36}
+                height={36}
+                className={'block shrink-0 cursor-pointer'}
+                onClick={closeSideModal}
+              />
+            </div>
             {Content && <Content />}
           </motion.div>
         </motion.section>
