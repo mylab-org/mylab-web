54d5323d86086ccea0fdcbabdbf9c7a3982abe2b
JinY <wlsdud6221@naver.com>
2026-09-19 02:06:48 +0900

    feat : Users, Auth 엔드포인트 및 쿼리키 정의

--- src/shared/api/endpoint.ts
+++ src/shared/api/endpoint.ts
@@ -1,6 +1,14 @@
 export const ENDPOINTS = {
-  USER: {
-    LOGIN: '/auth/login',
-    REFRESH: '/auth/refresh',
+  USERS: {
+    ROOT: '/api/users/me',
+    PASSWORD: '/api/users/me/password',
+  },
+  AUTH: {
+    REGISTER: '/api/auth/register',
+    LOGIN: '/api/auth/login',
+    LOGOUT: '/api/auth/logout',
+    VERIFY_EMAIL: '/api/auth/verify-email',
+    RESEND_VERIFICATION: '/api/auth/resend-verification',
+    REFRESH: '/api/auth/refresh',
   },
 }
