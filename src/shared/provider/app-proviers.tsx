4f4b62a511b4a8c450f5e90b67767a7ac436d6ce
JinY <wlsdud6221@naver.com>
2026-09-20 23:24:42 +0900

    feat : 로그아웃 적용

--- src/shared/provider/app-proviers.tsx
+++ src/shared/provider/app-proviers.tsx
@@ -1,7 +1,9 @@
 'use client'
 
+import { useRouter } from 'next/navigation'
+import { useEffect, type ReactNode } from 'react'
 import { QueryProvider } from './query-provider'
-import type { ReactNode } from 'react'
+import { registerLogoutHandler } from '../lib/token/client-access-token-store'
 
 // 루트에서 공통 provider를 묶는 진입점 예시입니다.
 type AppProvidersProps = {
@@ -9,5 +11,18 @@ type AppProvidersProps = {
 }
 
 export function AppProviders({ children }: AppProvidersProps) {
+  const router = useRouter()
+
+  useEffect(() => {
+    // interceptor 에서 세션 만료 시 앱 라우터로 메인 이동
+    registerLogoutHandler(() => {
+      router.replace('/')
+    })
+
+    return () => {
+      registerLogoutHandler(null)
+    }
+  }, [router])
+
   return <QueryProvider>{children}</QueryProvider>
 }
