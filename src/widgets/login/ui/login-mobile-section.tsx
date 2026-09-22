c6abb66a6ed5aeb4c0e105395714dc00eb0e7e4d
JinY <wlsdud6221@naver.com>
2026-09-23 02:43:09 +0900

    fix : 로그인 페이지 하위 섹션으로 분리

---
+++ src/widgets/login/ui/login-mobile-section.tsx
@@ -0,0 +1,40 @@
+'use client'
+
+import Image from 'next/image'
+import { useState } from 'react'
+import { LoginLabCreateJoinModal } from './modal/login-lab-create-join-modal'
+import { LoginForm } from '@/features/auth'
+import { Button } from '@/shared/ui/override/button'
+import { AuthLinkItem } from '@/shared/ui/template/auth-link-item'
+import { MobileAuthHeader } from '@/widgets/layout/header'
+
+export const LoginMobileSection = () => {
+  const [isLogin, setIsLogin] = useState(false)
+
+  return isLogin ? (
+    <>
+      <MobileAuthHeader isPrev onPrev={() => setIsLogin(false)} />
+      <div className={'flex flex-1 flex-col gap-7.5 px-7.5 pb-5'}>
+        <h1 className={'text-[22px] font-bold'}>로그인하기</h1>
+        <div className={'flex flex-1 flex-col gap-[30px]'}>
+          <LoginForm LabModal={LoginLabCreateJoinModal} />
+          <div className={'mt-3 space-y-3'}>
+            <AuthLinkItem title={'회원가입하러 가기'} text={'MyLab이 처음이신가요?'} href={'/signup'} />
+            <AuthLinkItem title={'비밀번호 재설정하기'} text={'비밀번호를 잊으셨나요?'} href={'#'} />
+          </div>
+        </div>
+      </div>
+    </>
+  ) : (
+    <div className={'flex flex-1 flex-col gap-7.5 px-7.5 pb-5'}>
+      <div className={'flex flex-1 flex-col items-center justify-center'}>
+        <Image src={'/MyLab_Logo.png'} alt={'MyLab'} width={60} height={60} />
+        <span className="text-[30px] font-bold text-gray-900">
+          My<b className={'text-[#2562CD]'}>Lab</b>
+        </span>
+        <span className="text-[15px] font-semibold text-gray-900">내 손안의 작은 연구실</span>
+      </div>
+      <Button onClick={() => setIsLogin(true)}>로그인</Button>
+    </div>
+  )
+}
